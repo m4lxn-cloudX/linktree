a946c0458e1fb857f985cf12d6157d724d131987
milan <milan.duettra@gmail.com>
2026-04-03 08:53:19 +0200

    Changes to be committed: 	modified:   README.md 	new file:   assets/css/general.css 	new file:   assets/css/main.css 	new file:   assets/img/Screenshot 2026-04-03 084831.png 	new file:   assets/img/individually/profile.jpeg 	new file:   assets/img/instagram.webp 	new file:   assets/js/fillin.js 	new file:   assets/js/main.js 	modified:   index.html

---
+++ assets/js/fillin.js
@@ -0,0 +1,44 @@
+//FILL IN USER-SPECIFIC DATA HERE! (e.g. name, profile picture, social media links, etc.)
+
+//Fill in your date here.
+const TEXT_REPLACEMENTS = {
+  'UserName': 'Milan',
+};
+
+//Fill in your profile picture path, here. If you have another data type, just type the data type. For example, profile.jpeg -> profile.png .
+const IMAGE_REPLACEMENTS = {
+  'ProfilePicture': './assets/img/individually/profile.jpeg',
+};
+
+//Fill in your social media locations here. Only edit after >>window.open('<< and stop before >>', '_blank');<<. 
+// For example, if your Instagram is https://www.instagram.com/milan_.1410/, then you would only paste your link in the underlined part >>window.open('https://www.instagram.com/milan_.1410/', '_blank');<<.
+const ONCLICK_REPLACEMENTS = {
+  'instagram': "window.open('https://www.instagram.com/milan_.1410/', '_blank');",
+};
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+//DO NOT EDIT BELOW THIS LINE!
+//IF YOU EDIT BELOW THIS LINE, YOUR CHANGES MAY BE OVERWRITTEN AND LOST FOREVER! (Unless you know what you're doing)
+document.addEventListener('DOMContentLoaded', () => {
+  for (const [id, value] of Object.entries(TEXT_REPLACEMENTS)) {
+    const el = document.getElementById(id);
+    if (el) el.textContent = value || id;
+  }
+
+  for (const [id, src] of Object.entries(IMAGE_REPLACEMENTS)) {
+    if (!src) continue;
+    const el = document.getElementById(id);
+    if (el) el.src = src;
+  }
+
+  for (const [id, handler] of Object.entries(ONCLICK_REPLACEMENTS)) {
+    if (!handler) continue;
+    const el = document.getElementById(id);
+    if (el) el.setAttribute('onclick', handler);
+  }
+});
